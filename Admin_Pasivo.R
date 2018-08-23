04a4e74c5eac0279c620f2940b829d94f6b3c161
EduardoRivas96 <35493260+EduardoRivas96@users.noreply.github.com>
2018-08-23 08:15:11 -0500

    Update Admin_Pasivo.R

--- Admin_Pasivo.R
+++ Admin_Pasivo.R
@@ -1,47 +1,112 @@
-# -- Laboratorio 1
+#Laboratorio 1
+# Remover todos los objetos del "Environment"
 
-#Remover todo los objetos del environment
-rm(list= ls())
+rm(list = ls())
 
-# los 0s aceptadosantes de expresar una cifra en notación científica
+
+
+# los 0s aceptados antes de expresas una cifra en notaci?n cient?fica
+
 options("scipen"=100, "digits"=4)
 
-###Librerías a Utilizar
-suppressMessages(library(plotly)) #Gráficas interactivas
-suppressMessages(library(Quandl)) #Descargar Prescios
-suppressMessages(library(PortfolioAnalytics))#
-suppressMessages(library(ROI))
-suppressMessages(library(kableExtra))
-options(knitr.table.format = "html")  
 
-# Cargar el token de QUANDL 
+
+### Cargas librer?as a utilizar
+
+suppressMessages(library(plotly)) # Graficas interactivas
+
+suppressMessages(library(Quandl)) # Descargar Precios
+
+suppressMessages(library(PortfolioAnalytics)) # Teor?a Moderna de Portafolios
+
+suppressMessages(library(ROI)) # Optimizacion para portafolio
+
+
+
+suppressMessages(library(knitr))  # Opciones de documentaci?n + c?digo
+
+suppressMessages(library(kableExtra)) # Tablas en HTML
+
+options(knitr.table.format = "html") 
+
+
+
+# Cargar el token de QUANDL
+
 Quandl.api_key("PgavWKzHY7L8AwGkLY2e")
 
-#Función para descargar precios
-Bajar_precios <-function(Columns, Ticker, Fecha_In, Fecha_Fn) {
+
+
+# Funcion para descagar precios
+
+Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
   
-  # Funcion para descargar N cantidad de activos desde QUANDL 
-  # -- Dependencias: QUANDL 
-# -- Columns : columnas a incluir : character : c("date", "adj_close", ... ) 
-# -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA" 
-# -- Fecha_In : Fecha Inicial : character : "2017-01-02" 
-# -- Fecha_Fn : Fecha Final : character : "2017-08-02" 
   
-  #Descargar Precios
   
-  Datos <- Quandl.datatable(code = "WIKI/PRICES", qopts.columns=Columns, ticker=Ticker, date.gte=Fecha_In, date.lte=Fecha_Fn)
+  # Funcion para descargar N cantidad de activos desde QUANDL
+  
+  # -- Dependencias: QUANDL
+  
+  # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
+  
+  # -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA"
+  
+  # -- Fecha_In : Fecha Inicial : character : "2017-01-02"
+  
+  # -- Fecha_Fn : Fecha Final : character : "2017-08-02"
+  
+  
+  
+  # Peticion para descargar precios
+  
+  Datos <- Quandl.datatable("WIKI/PRICES", qopts.columns=Columns, ticker=Tickers,
+                            
+                            date.gte=Fecha_In, date.lte=Fecha_Fn)
+  
+  return(Datos)
+  
+}
 
-return(Datos)}
 
-#Tikers de acciones y datos a solicitar a Quandl
 
-tk<- c("TSLA", "AAPL", "SPY")
-cs<- c("date", "adj_close")
+# Tickers de accciones y datos a solicitar a QUANDL
 
-fs<- c("2015-08-01", "2016-08-01")
+tk <- c("TSLA", "BBY", "HD")
 
-Datos<- list()
+cs <- c("date", "adj_close")
 
-for(i in 1:length(tk)){ Datos[[i]]<- Bajar_precios(Columns =cs, Ticker=tk[i], Fecha_In=fs[1], Fecha_Fn = fs[2])}
 
-names(Datos)<- tk
+
+# Fecha inicial y fecha final
+
+fs <- c("2015-08-01", "2017-08-01")
+
+
+
+# Descargar Precios y Calcular rendimientos
+
+Datos <- list()
+
+
+
+for(i in 1:length(tk))
+  
+  Datos[[i]] <- Bajar_Precios(Columns=cs, Ticker=tk[i], Fecha_In=fs[1], Fecha_Fn=fs[2])
+
+
+
+names(Datos) <- tk
+
+
+
+for(i in 1:length(tk))
+  
+  Datos[[i]]$adj_close_r <- c(0, diff(log(Datos[[i]]$adj_close)))
+
+
+
+Rends <- xts(x = cbind(Datos[[1]]$adj_close_r, Datos[[2]]$adj_close_r, Datos[[3]]$adj_close_r),
+             
+             order.by = Datos[[1]]$date)[-1]
+
+names(Rends) <- tk
