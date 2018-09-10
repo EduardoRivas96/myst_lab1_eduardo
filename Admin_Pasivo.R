c88ad5f7d29aaeddf15b290625dba0b591a8a39d
EduardoRivas96 <35493260+EduardoRivas96@users.noreply.github.com>
2018-09-10 07:39:43 -0500

    Update Admin_Pasivo.R

--- Admin_Pasivo.R
+++ Admin_Pasivo.R
@@ -1,14 +1,13 @@
 #Laboratorio 1
+
 # Remover todos los objetos del "Environment"
 
 rm(list = ls())
 
 
-
 # los 0s aceptados antes de expresas una cifra en notaci?n cient?fica
 
 options("scipen"=100, "digits"=4)
-
 
 
 ### Cargas librer?as a utilizar
@@ -21,8 +20,6 @@ suppressMessages(library(PortfolioAnalytics)) # Teoría Moderna de Portafolios
 
 suppressMessages(library(ROI)) # Optimizacion para portafolio
 
-
-
 suppressMessages(library(knitr))  # Opciones de documentaci?n + c?digo
 
 suppressMessages(library(kableExtra)) # Tablas en HTML
@@ -30,66 +27,111 @@ suppressMessages(library(kableExtra)) # Tablas en HTML
 options(knitr.table.format = "html") 
 
 
-
 # Cargar el token de QUANDL
+
 
 Quandl.api_key("PgavWKzHY7L8AwGkLY2e")
 
 
 
+
+
+
+
 # Funcion para descagar precios
+
+
 
 Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
   
   
   
+  
+  
+  
+  
   # Funcion para descargar N cantidad de activos desde QUANDL
+  
+  
   
   # -- Dependencias: QUANDL
   
+  
+  
   # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
+  
+  
   
   # -- Tickers : Tickers o claves de pizarra de los activos : character : "TSLA"
   
+  
+  
   # -- Fecha_In : Fecha Inicial : character : "2017-01-02"
+  
+  
   
   # -- Fecha_Fn : Fecha Final : character : "2017-08-02"
   
   
   
+  
+  
+  
+  
   # Peticion para descargar precios
+  
+  
   
   Datos <- Quandl.datatable("WIKI/PRICES", qopts.columns=Columns, ticker=Tickers,
                             
+                            
+                            
                             date.gte=Fecha_In, date.lte=Fecha_Fn)
   
+  
+  
   return(Datos)
+  
+  
   
 }
 
 
 
+
 # Tickers de accciones y datos a solicitar a QUANDL
 
-tk <- c("TSLA", "BBY", "HD")
+
+
+tk <- c("AMZN", "AAPL", "HRS")
+
+
 
 cs <- c("date", "adj_close")
 
 
 
+
 # Fecha inicial y fecha final
 
-fs <- c("2015-08-01", "2017-08-01")
 
+
+fs <- c("2015-08-01", "2017-08-01")
 
 
 # Descargar Precios y Calcular rendimientos
 
 Datos <- list()
 
+
+
 Capital_Inicial<-10000
 
+
+
 for(i in 1:length(tk))
+  
+  
   
   Datos[[i]] <- Bajar_Precios(Columns=cs, Ticker=tk[i], Fecha_In=fs[1], Fecha_Fn=fs[2])
 
@@ -99,70 +141,124 @@ names(Datos) <- tk
 
 
 
+
 for(i in 1:length(tk))
+  
+  
   
   Datos[[i]]$adj_close_r <- c(0, diff(log(Datos[[i]]$adj_close)))
 
 
-
 Rends <- xts(x = cbind(Datos[[1]]$adj_close_r, Datos[[2]]$adj_close_r, Datos[[3]]$adj_close_r),
+             
+             
              
              order.by = Datos[[1]]$date)[-1]
 
+
+
 names(Rends) <- tk
+
+
 
 Port1<- portfolio.spec(assets = tk) 
 
+
+
 #Especificar restricciones del portafolio
+
 #1era restriccion, invertir el 100% del capital en el portafolio
+
+
 
 Port1<- add.constraint(portfolio = Port1, type = "full_investment")
 
+
+
 #limites superior e inferior de los pesos individuales
+
 Port1<- add.constraint(portfolio = Port1,
+                       
                        type = "box",min= c(0.01, 0.01, 0.01), max= c(0.7, 0.7, 0.7))
+
+
 
 Port1<- add.objective(portfolio = Port1, type = "return", name = "mean")
 
+
+
 Port1<- optimize.portfolio(R= Rends, portfolio = Port1, optimize_method = "random",
+                           
                            trace=TRUE, search_size = 500)
+
+
 
 Portafolios<- vector("list", length= length(Port1$random_portfolio_objective_results))
 
+
+
 for(i in 1:length(Port1$random_portfolio_objective_results))
+  
+  
   
   Portafolios[[i]]$Pesos <- Port1$random_portfolio_objective_results[[i]]$weights #Doble corchete para tomar un elemento de una LISTA
 
-  Portafolios[[i]]$Medias <- Port1$random_portfolio_objective_results[[i]]$objective_measures$mean
+
+
+Portafolios[[i]]$Medias <- Port1$random_portfolio_objective_results[[i]]$objective_measures$mean
+
+
+
+Portafolios[[i]]$Vars <- var.portfolio(R= Port1$R, weights = Portafolios[[i]]$Pesos)
+
+
+
+##names(Portafolios[[i]]$Medias)<- NULL
+
+
+
+df_Portafolios <-data.frame(matrix(nrow=length(Port1$random_portfolio_objective_results),ncol=3, data=0))
+
+
+
+colnames(df_Portafolios)<-c("Rend", "Var", "Clase")
+
+
+
+for(i in 1:lenght(Port1$random_portfolio_objective_results)){
   
-  Portafolios[[i]]$Vars <- var.portfolio(R= Port1$R, weights = Portafolios[[i]]$Pesos)
+  i<-2
   
-  ##names(Portafolios[[i]]$Medias)<- NULL
   
-  df_Portafolios <-data.frame(matrix(nrow=length(Port1$random_portfolio_objective_results),ncol=3, data=0))
   
-  colnames(df_Portafolios)<-c("Rend", "Var", "Clase")
+  df_Portafolios$Rend[i]<- round(Portafolios[[i]]$Medias*252,4) #Un corchete cuando se selecciona un elemento individual
   
-  for(i in 1:lenght(Port1$random_portfolio_objective_results)){
-    i<-2
+  
+  
+  df_Portafolios$Var[i]<- round(sqrt(Portafolios[[i]]$Vars)*sqrt(252),4) 
+  
+  
+  
+  df_Portafolios$Clase[i]<- "No-Frontera"
+  
+  
+  
+  for(k in 1:lenght(tk)){
     
-    df_Portafolios$Rend[i]<- round(Portafolios[[i]]$Medias*252,4) #Un corchete cuando se selecciona un elemento individual
+    k<-2
     
-    df_Portafolios$Var[i]<- round(sqrt(Portafolios[[i]]$Vars)*sqrt(252),4) 
     
-    df_Portafolios$Clase[i]<- "No-Frontera"
     
-    for(k in 1:lenght(tk)){
-      k<-2
-      
     df_Portafolios[i,paste("Peso_", tk[k], sep="")]<- Portafolios[[i]]$Pesos[k]
-    
+   
     df_Portafolios[i,paste("Titulos_ini_", tk[k], sep="")]<- 
+      
       (Capital_Inicial*Portafolios[[i]]$Pesos[k])%/%Datos[[k]]$adj_close[1]
     
-    }
+    
     
   }
-    
   
   
+  
+}
