d64edcdc86111482de792fd9d8c8ad28de6eb67c
EduardoRivas96 <35493260+EduardoRivas96@users.noreply.github.com>
2018-10-04 07:49:38 -0500

    Update Admin Activa.R

--- Admin Activa.R
+++ Admin Activa.R
@@ -19,7 +19,7 @@ suppressMessages(library(Quandl)) # Descargar Precios
 suppressMessages(library(plyr))
 suppressMessages(library(ROI))    # Optimizacion para portafolio
 suppressMessages(library(knitr))  # Opciones de documentaci?n + c?digo
-suppressMessages(library(xlsx))   # Leer archivos XLSx
+suppressMessages(library(openxlsx))   # Leer archivos XLSx
 suppressMessages(library(kableExtra)) # Tablas en HTML
 suppressMessages(library(PortfolioAnalytics)) # Teor?a Moderna de Portafolios
 
@@ -35,9 +35,9 @@ Quandl.api_key("PgavWKzHY7L8AwGkLY2e ")
 
 
 Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
- 
+  
   # Funcion para descargar N cantidad de activos desde QUANDL
- 
+  
   # -- Dependencias: QUANDL
   
   # -- Columns : columnas a incluir : character : c("date", "adj_close", ... )
@@ -54,28 +54,22 @@ Bajar_Precios <- function(Columns, Tickers, Fecha_In, Fecha_Fn) {
   Datos <- Quandl.datatable( "WIKI/PRICES", qopts.columns=Columns,
                              
                              ticker=Tickers,
-
+                             
                              date.gte=Fecha_In, date.lte=Fecha_Fn)
-
+  
   return(Datos)
   
 }
 
 # Tickers de acciones contenidas en ETF-HACK
 
-tk <- as.data.frame(read.xlsx(file = "ITOT.xlsx",
+tk <- as.data.frame(read.xlsx( xlsxFile = "C:/Users/lalor/Desktop/Otoño 2018/TRADING/ITOT.xlsx",
                               
-                              sheetName = "ITOT",
+                              sheet = "ITOT",
+                              startRow = 10,
                               
-                              colIndex=1,
-                              
-                              
-                              
-                              startRow=10,
-                              
-                              
-                              
-                              endRow=73,header = FALSE))
+                          
+                              ))
 
 
 
@@ -166,7 +160,6 @@ DatosN <- Datos[completos]
 columnas <- c()
 
 
-
 nuevos   <- c()
 
 
@@ -210,29 +203,26 @@ tk_completos <- as.character(tk[completos])
 
 colnames(Precios) <- tk_completos
 
-
-
 # -- ----------------------------------------------------------------------------------------- -- #
 
 # -- ----------------------------------------------------------------------------------------- -- #
 
 # -- ----------------------------------------------------------------------------------------- -- #
-
-
 
 Historico <- data.frame("Date" = row.names(Precios),
                         
                         "Precio" = Precios[,1], 
+                        
                         "R_Precio" = 0, 
-        
+                        
                         "R_Activo" = 0,
-   
+                        
                         "R_Cuenta" = 0, 
-
-                        "Capital" = 0, "Balance" = 0, "Titulos" = 0,
- 
+                        
+                        "Capital" = 0, "Flotante"=0, "Balance" = 0, "Titulos" = 0,
+                        
                         "Titulos_a" = 0,
-
+                        
                         "Operacion" = NA, "Comisiones" = 0, "Mensaje" = NA)
 
 # *Date*       : Fecha (Proviene desde los precios que bajaron).
@@ -245,7 +235,9 @@ Historico <- data.frame("Date" = row.names(Precios),
 
 # *Capital*    : El dinero no invertido (Equivalente a Efectivo).
 
-# *Balance*    : El valor del portafolio (Precio diario X Titulos).
+# *Balance*    : El valor del portafolio (Precio diario X Titulos + Capital).
+
+# "Flotante"   : EL valor de los titulos por el precio sin capital (Precio diario x Titulos)
 
 # *R_Cuenta*   : Balance + Capital (Cada dia respecto al capital inicial).
 
@@ -294,6 +286,9 @@ Historico$Comisiones[1] <- Historico$Titulos[1]*Historico$Precio[1]*Regla4_C
 
 Historico$Balance[1] <- Historico$Titulos[1]*Historico$Precio[1]
 
+#--Calcular el Valor Flotante de la posición
+Historico$Capital[1]<-Historico$Titulos_a[1]*Historico$Precio[1]
+
 # -- Todo remanente se dejar? registrado en la cuenta de efectivo.
 
 Historico$Capital[1] <- Regla5_K-Historico$Balance[1]-Historico$Comisiones[1]
@@ -310,8 +305,6 @@ Historico$R_Cuenta[1] <- 0
 
 # -- Mensaje inicial
 
-
-
 Historico$Mensaje[1] <- "Inicializacion de cartera"
 
 # -- Calcular R_Precio
@@ -321,9 +314,9 @@ Historico$R_Precio <- round(c(0, diff(log(Historico$Precio))),4)
 # -- Calcular R_Activo
 
 for(i in 1:length(Historico$Date)){
-
+  
   Historico$R_Activo[i] <- round((Historico$Precio[i]/Historico$Precio[1])-1,2)
-
+  
 }
 
 
@@ -334,42 +327,42 @@ for(i in 1:length(Historico$Date)){
 # -- ------------------------------------ -- #
 
 for(i in 2:length(Historico$Date)){
-
-  if(Historico$R_Precio[i] <= Regla0_R){ # Generar Senal
-
-    # Establecer capital actual, inicialmente, igual al capital anterior
-
-    Historico$Capital[i] <- Historico$Capital[i-1]
-
-    if(Historico$Capital[i] > 0){ # Si hay capital
   
-      if(Historico$Capital[i]*Regla2_P > Historico$Precio[i]){ # Si Capital minimo
+  if(Historico$R_Precio[i] <= Regla0_R){ # Generar Senal
     
+    # Establecer capital actual, inicialmente, igual al capital anterior
+    
+    Historico$Capital[i] <- Historico$Capital[i-1]
+    
+    if(Historico$Capital[i] > 0){ # Si hay capital
+      
+      if(Historico$Capital[i]*Regla2_P > Historico$Precio[i]){ # Si Capital minimo
+        
         Historico$Operacion[i] <- "Compra"
-   
+        
         Historico$Titulos[i]   <- (Historico$Capital[i]*Regla2_P)%/%Historico$Precio[i]
-
+        
         compra <- Historico$Precio[i]*Historico$Titulos[i]  
-
+        
         Historico$Comisiones[i] <- compra*Regla4_C
-
+        
         
         Historico$Titulos_a[i] <- Historico$Titulos[i-1]+Historico$Titulos[i]
         Historico$Mensaje[i] <- "Compra Exitosa"
         
       }
-
+      
       
     }
-
+    
     else { # No hubo capital
-
+      
     }
-
+    
   }
-
+  
   else { # Sin senal
-
+    
   }
-
+  
 }
